7ead535d06d6d78ce31336d9926c1e83de604033
Ekaterina Andriushina <ekaterina_andriushina@epam.com>
2022-07-20 22:24:55 +0300

    add sql scripts to add data to tables

---
+++ insertTableData.sql
@@ -0,0 +1,19 @@
+insert into products (title, description, price) values
+('Element', 'Pow Supply Co 8.25 Plant Blue Complete', 70),
+('Face T', 'Pow Supply Co 8,25 Face T Fade Complete Kaykay', 55),
+('Screaming Hand', 'Santa Cruz 7,8 Screaming Hand Complete Kaykay', 72),
+('Deathcard', 'Creature 8,25 Deathcard Complete Kaykay', 59),
+('Dirty P Colony', 'Primitive 8,0 Dirty P Colony Complete Kaykay', 92),
+('Dgk Juicy', 'Dgk 8,0 Dgk Juicy Complete Kaykay', 65),
+('Dgk Scribble', 'Dgk 8,0 Dgk Scribble Complete Kaykay', 59),
+('Rough Original', 'Plan B 8,0 Rough Original Complete Kaykay', 72)
+
+insert into stocks (product_id, count) values
+('79b9366a-404a-44c3-b0dc-76b30d2f4c5a', 4),
+('1d21cb3a-6376-4b18-91d1-64c1d7d11f74 T', 6),
+('14c74cd4-ba68-49f6-b98b-dcd85849664c', 7),
+('7186ccee-a3f2-45cc-acf5-1ada7e45c376', 10),
+('f4d7bcc4-7698-4e1d-a4bf-05c9d6c2eaec', 3),
+('aaf531b0-6c25-4934-982b-a23d62da5b6c', 12),
+('88de0191-96af-4dad-8c7a-99328257ca70', 15),
+('c90cffa0-a92a-49c2-bfe2-e46bf215b6b2', 9)
