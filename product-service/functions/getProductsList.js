8a459726e4162b20d5abfa395021b671b713eefa
Ekaterina Andriushina <ekaterina_andriushina@epam.com>
2022-07-21 22:08:15 +0300

    create endpoint for post /products

--- product-service/functions/getProductsList.js
+++ product-service/functions/getProductsList.js
@@ -1,8 +1,27 @@
-import axios from "axios";
+import { Client } from 'pg';
+
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+const dbOptions = {
+  host: PG_HOST,
+  port: PG_PORT,
+  database: PG_DATABASE,
+  user: PG_USERNAME,
+  password: PG_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  connectionTimeoutMillis: 5000
+};
 
 const getProductsList = async () => {
+  console.log('New request started');
+  const client = new Client(dbOptions);
+  await client.connect();
+
   try {
-    const { data: products } = await axios.get('https://kb68k4fkw8.execute-api.eu-west-1.amazonaws.com/skates');
+    const { rows: products } = await client.query(`
+      select id, title, description, price, count from products inner join stocks on id = product_id
+    `);
 
     return {
       body: products,
@@ -17,6 +36,8 @@ const getProductsList = async () => {
       statusCode: 500,
       error: error.message,
     }
+  } finally {
+    client.end();
   }
 };
 
